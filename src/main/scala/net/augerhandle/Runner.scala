2cc9ca60d8e4e656a1eb593830fe3b826a7c5675
Hasan, David <HasanD@advisory.com>
2017-08-28 15:10:10 -0500

    add several flows

--- src/main/scala/net/augerhandle/Runner.scala
+++ src/main/scala/net/augerhandle/Runner.scala
@@ -15,22 +15,62 @@ class Runner( actorSystem : ActorSystem ) {
   def run() 
   {
     // Create a "source" containing a bunch of integers.
-    val source = Source(1 to 10)
+    val integerSource = Source(1 to 10)
 
     // Create a "flow" that adds a big number to the incoming integers, returning an integer.
-    val flow1 = Flow[Int].map(i => i + 1000) 
+    val incrementingFlow = Flow[Int].map(
+      i_in => 
+        {
+          val i_out = i_in + 1000
+          log.debug( s"incrementing flow: i_in=${i_in}, i_out=${i_out}")
+          i_out
+        }
+      ) 
 
     // Create another "flow" that only lets even incoming values pass thru.
-    val flow2 = Flow[Int].filter( i => i%2==0)
+    val filterOutOddNumbersFlow = Flow[Int].filter( 
+      i => 
+        {
+          val isEven = (i % 2) == 0
+          if( isEven )
+          {
+            log.debug( s"even number, ${i}, permitted to pass thru" )
+            true
+          }
+          else
+          {
+            log.debug( s"odd number, ${i}, dropped from the flow")
+            false
+          }
+        }
+      )
 
-    // Create a "sink" that adds all the incoming values.
-    val sink = Sink.foreach[Int](i => log.info(s"item being processed: ${i}"))
+    // Create another "flow" that calculates a running some of all the incoming values.
+    val addingFlow = Flow[Int].fold(0)(
+      (accum,i_in) =>
+        {
+          val sum_out = accum + i_in
+          log.debug( s"running sum=${sum_out}" )
+          sum_out
+        }
+      )
+
+    // Create yet another "flow" that converts the incoming value into an outgoing string
+    val outputFormattingFlow = Flow[Int].collect( 
+      { 
+        case i_in => s"sum=${i_in}" 
+      })
+
+    // Create a "sink" that logs all the incoming values.
+    val loggingSink = Sink.foreach[String](s_in => log.info(s"item being processed:${s_in}"))
 
     val runnableGraph = 
-      source
-        .via(flow1)
-        .via(flow2)
-        .to(sink)
+      integerSource
+        .via(incrementingFlow)
+        .via(filterOutOddNumbersFlow)
+        .via(addingFlow)
+        .via(outputFormattingFlow)
+        .to(loggingSink)
 
     runnableGraph.run()(actorMaterializer)
   }
